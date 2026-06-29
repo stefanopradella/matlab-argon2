87c642ea3e9132f3df90f1fac4eaf92bbbe30fdb
stefanopradella <stefano.pradella.97@gmail.com>
2026-06-29 14:23:08 +0200

    Add entry-point functions for Argon2i and Argon2d

---
+++ argon2d.m
@@ -0,0 +1,75 @@
+function tag = argon2d(P, S, p, T, m, t, K, X)
+% argon2d - Compute the tag of P using the Argon2d algorithm
+% 
+% Syntax: 
+%   tag = ARGON2D(P, S, p, T, m, t, K, X)
+%   tag = ARGON2D(P, S, p, T, m, t)
+% 
+% Input Arguments
+%   P - Message string
+%       string | char array | uint8
+%   S - Nonce
+%       string | char array | uint8
+%   p - Parallelism: number of lanes
+%       positive integer scalar between 1 and 2^24 - 1
+%   T - Tag length in bytes
+%       positive integer scalar between 4 and 2^32 - 1
+%   m - Memory size in KiB
+%       positive integer scalar between 8*p and 2^32 - 1
+%   t - Number of passes
+%       positive integer scalar between 1 and 2^32 - 1
+%   K - Secret value
+%       string | char array | uint8
+%   X - Associated data
+%       string | char array | uint8
+% 
+% Reference: 
+%   https://datatracker.ietf.org/doc/html/rfc9106.html
+
+    arguments
+        P   (:, 1)  char                            % Message string
+        S   (:, 1)  char                            % Nonce
+        p   (1, 1)  double {mustBeInteger}          % Parallelism: number of lanes
+        T   (1, 1)  double {mustBeInteger}          % Tag length
+        m   (1, 1)  double {mustBeInteger}          % Memory size [KiB]
+        t   (1, 1)  double {mustBeInteger}          % Number of passes
+        K   (:, 1)  char = []                       % Secret value
+        X   (:, 1)  char = []                       % Associated data
+    end
+
+    pp      = p;
+    tt      = t;
+    ll      = numel(P);
+    sl      = numel(S);
+    kk      = numel(K);
+    xx      = numel(X);
+    max32   = 2^32 - 1;
+
+    % Input validation
+    if ~allbetween(ll, 0, max32)
+        error("Message string length must be between 0 and 2^32 - 1")
+    end
+    if ~allbetween(sl, 8, max32)
+        error("Nonce length must be between 8 and 2^32 - 1")
+    end
+    if ~allbetween(pp, 1, 2^24 - 1)
+        error("Parallelism must be between 1 and 2^24 - 1")
+    end
+    if ~allbetween(T, 4, max32)
+        error("Tag length must be between 4 and 2^32 - 1")
+    end
+    if ~allbetween(m, 8*pp, max32)
+        error("Memory size must be between 8*p and 2^32 - 1")
+    end
+    if ~allbetween(tt, 1, max32)
+        error("Number of passes must be between 1 and 2^32 - 1")
+    end
+    if ~allbetween(kk, 0, max32)
+        error("Secret value length must be between 0 and 2^32 - 1")
+    end
+    if ~allbetween(xx, 0, max32)
+        error("Associated data length must be between 0 and 2^32 - 1")
+    end
+
+    tag = argon2impl.argon2(P, S, p, T, m, t, 0, K, X);
+end
